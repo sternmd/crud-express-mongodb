5f54ddc2ba52c8fabeeaff15286b21eabd2db974
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-06-20 11:28:30 -0500

    form actions

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Handlers
 app.listen(3000, function () {
     console.log('listening on 3000')
 })
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })

d9349bb17a8028ce9f5efc25db6ece0e538d04b2
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-06-20 11:48:38 -0500

    setup mongodb

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// Handlers
-app.listen(3000, function () {
-    console.log('listening on 3000')
+// MongoDB
+var url = process.env.MONGOLAB_URL;
+var db;
+
+MongoClient.connect(url, (err, database) => {
+    if (err) return console.log(err)
+    db = database.db('crud-express-example')
+
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
 })
+
+// Handlers
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
